7864b4de6195aefa802d312f56537656e484345c
zhiguo <zhiguo8@staff.weibo.com>
2025-09-13 00:45:32 +0800

    pass vim-gutentags.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,27 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate", -- Command to run after installation to update parsers
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "c",
-                    "python",
-                    "json",
-                    "bash",
-                    "yaml",
-                    "lua",
-                    "vim",
-                    "javascript",
-                    "markdown",
-                    "markdown_inline",
-                }, -- Add languages you want parsers for
-                sync_install = false,
-                auto_install = true, -- Automatically install missing parsers
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true },
-            })
-        end,
-    },
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate", -- Command to run after installation to update parsers
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "c",
+                "python",
+                "json",
+                "bash",
+                "yaml",
+                "lua",
+                "vim",
+                "javascript",
+                "markdown",
+                "markdown_inline",
+            }, -- Add languages you want parsers for
+            sync_install = false,
+            auto_install = true, -- Automatically install missing parsers
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+        })
+    end,
 }
